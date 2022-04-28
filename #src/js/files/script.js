538e10b8400ddca64e44eccd2ceb41001fedc4a8
IvanGrishov <ivangrishov@gmail.com>
2022-04-28 15:02:08 +0300

    new changes

--- #src/js/files/script.js
+++ #src/js/files/script.js
@@ -1,7 +1,23 @@
 $(document).ready(function() {
+
     $('.header__catalog-btn').on('click', function() {
         $(this).toggleClass('header__catalog-btn--active')
         $('body').toggleClass('catalog--active')
     })
+
+    $('.header__sign-in').click(function(){
+        $.fancybox.open({
+            src: '.sign-in-modal',
+            type: 'inline'
+        });
+    });
+
+    $('.header__sign-up').click(function(){
+        $.fancybox.open({
+            src: '.sign-up-modal',
+            type: 'inline'
+        });
+    });
+
 })
 
--- #src/js/vendors.js
+++ #src/js/vendors.js
@@ -1,3 +1,3 @@
 @@include('libs/jquery-3.6.0.min.js', {})
 @@include('libs/swiper.min.js', {})
-
+@@include('libs/jquery.fancybox.min.js', {})
