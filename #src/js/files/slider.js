9c2e2bbb7eb702ff51aa7e451e8b1a5db95c6c0a
IvanGrishov <ivangrishov@gmail.com>
2022-04-29 10:23:45 +0300

    added cart page

--- #src/js/files/slider.js
+++ #src/js/files/slider.js
@@ -101,3 +101,48 @@ document.addEventListener("DOMContentLoaded",()=> {
         window.swiper(SLIDER_CLASS, SLIDER_SETTINGS);
     }
 });
+
+/// слайдер "c этим товаром покупают"
+document.addEventListener("DOMContentLoaded",()=> {
+    const SLIDER_NAME = 'also-buy-slider';
+    const SLIDER_CLASS = '.' + SLIDER_NAME;
+
+    if (document.querySelector(SLIDER_CLASS) !== null) {
+        const SLIDER_SETTINGS = {
+            init: true,
+            loop: true,
+            loopedSlides: 0,
+            grabCursor: true,
+            // autoplay: {
+            // 	delay: 5000,
+            // 	pauseOnMouseEnter: true
+            // },
+            navigation: {
+                nextEl: '.js-' + SLIDER_NAME + '-next',
+                prevEl: '.js-' + SLIDER_NAME + '-prev',
+                disabledClass: SLIDER_NAME + '__navigation-item_disabled'
+            },
+            pagination: {
+                bulletClass: SLIDER_NAME +'__pagination-item',
+                bulletActiveClass: SLIDER_NAME +'__pagination-item_active',
+                clickable: true,
+                clickableClass:  SLIDER_NAME +'__pagination-item_clickable',
+                el: '.' + SLIDER_NAME +'__pagination'
+            },
+            preloadImages: false,
+            slidesPerView: 4,
+            spaceBetween: 24,
+            freeMode: false,
+            containerModifierClass: SLIDER_NAME +'_',
+            wrapperClass: SLIDER_NAME + '__wrapper',
+            slideClass: SLIDER_NAME + '__item',
+            slideActiveClass: SLIDER_NAME + '__item_active',
+            slideDuplicateActiveClass: SLIDER_NAME + '__item_duplicate_active',
+            slideVisibleClass: SLIDER_NAME + '__item_visible',
+            slideDuplicateClass: SLIDER_NAME + '__item_duplicate',
+            breakpoints: {}
+        };
+        window.swiper(SLIDER_CLASS, SLIDER_SETTINGS);
+    }
+});
+
